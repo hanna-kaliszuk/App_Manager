eca3dbf8b5698f1271c8b9c4bb3a69205d1f6779
hkaliszuk <h.kaliszuk@student.uw.edu.pl>
2026-09-15 23:11:51 +0200

    proper secret key read from hidden folder

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,9 +1,12 @@
+from dotenv import load_dotenv
 from pathlib import Path
+
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
