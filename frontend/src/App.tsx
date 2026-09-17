7ebee2b48a285a4a13cc8bd0500bce06391219a5
hkaliszuk <h.kaliszuk@student.uw.edu.pl>
2026-09-17 16:39:52 +0200

    added basic frontend pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Register from './pages/Register'
+import LogIn from './pages/LogIn'
+import Welcome from './pages/Welcome'
+import Jobs from './pages/Jobs'
+import Studies from './pages/Studies'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/welcome" element={<Welcome />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/studies" element={<Studies />} />
       </Routes>
     </BrowserRouter>
   )
